c1c41b86c851c314d44f9ba0b69774fc88574149
root <root@localhost.localdomain>
2018-10-05 16:30:40 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Game from './Game';
+import Logo from './logo.svg'
+import './App.css';
+
+class App extends Component {
+    
+    render() {
+	return (
+	    <div className='App-header'>
+	      <img src={Logo} className='App-logo'/>
+	      <h1> MobaTapTaupe </h1>
+	      <h3> Le but est de cliquer sur le bouton ROUGE </h3>
+	      <div>
+		<Game/>
+	      </div>
+	    </div>
+	);
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
+
+export default App;
---
+++ src/Game.js
@@ -0,0 +1,138 @@
+import React, {Component} from 'react';
+import blackBall from './boule_noire.svg'
+import redBall from './boule_rouge.svg'
+
+var startTime = 2;
+var nbTurn=1;
+var Activated=false;
+
+class Game extends Component {
+
+    constructor() {
+	super()
+	this.state = {
+	    count: startTime,
+	    turn: nbTurn,
+	    buttonOn: false,
+	    previous1: nbTurn,
+	    previous2: 0,
+	    random: 1
+	}
+    }
+
+    tick () {
+	if (this.state.count === 0) {
+	    this.setBackToZero();
+	    Activated = false;
+	    this.setState({buttonOn: false});
+	}
+	else {
+	    this.setState({count: (this.state.count - 1)})
+	}
+    }
+    
+    startTimer () {
+	Activated = true;
+	this.setState({buttonOn: true})
+	clearInterval(this.timer)
+    	this.timer = setInterval(this.tick.bind(this), 1000)
+    }
+    
+    stopTimer () {
+	clearInterval(this.timer)
+    }
+    
+    resetTimer () {
+	let a = this.state.previous1 + this.state.previous2;
+	
+	if (this.state.turn < 10) {
+	    startTime++;
+	}
+	nbTurn++;
+	this.setState({
+	    count: startTime,
+	    turn: nbTurn,
+	    previous1: a,
+	    previous2: this.state.previous1,
+	    random: getRandom(1, this.state.previous1)
+	});
+    }
+
+    activateButton() {
+	this.setState({buttonOn: false});
+	if (!Activated) {
+	    this.startTimer();
+	    this.setBackToZero();
+	}
+    }
+    
+    setBackToZero() {
+	startTime = 2;
+	nbTurn = 1;
+	this.setState({
+	    count: startTime,
+	    turn: nbTurn,
+	    previous1: nbTurn,
+	    previous2: 0,
+	    random: 1
+	});
+    }
+
+    componentWillUnmount() {
+	clearInterval(this.timer)
+    }
+    
+    render() {
+	let status;
+	let nbTurn;
+	let nbBowl;
+	let rand;
+	let balls=[];
+	const timer = this.state.count;
+	const turn = this.state.turn;
+
+	let button=[];
+
+	if (Activated) {
+	    status = 'Temps : ' + timer;
+	    nbTurn = 'Tour : ' + turn;
+	    nbBowl = 'N°boules : ' + this.state.previous1;
+	    rand = 'Random = ' + this.state.random + '/' + this.state.previous1;
+	    
+	}
+	if (this.state.buttonOn) {
+	    button=[];
+	    for (let index = 1; index <= this.state.previous1; index++) {
+		if (index === this.state.random) {
+		    balls.push(<div onClick={() => this.resetTimer()}className='RedBall'/>);
+		}
+		else {
+		    balls.push(<div className='BlackBall'/>);
+		}
+	    }
+	}
+	else {
+	    button.push(<button className='StartBut' onClick={() => this.activateButton()}>C'est parti </button>);
+	}
+	return (
+	    <div className='App-body'>
+	      <p>{status}</p>
+	      <p>{nbTurn}</p>
+	      <p>{nbBowl}</p>
+	      <br/>
+	      <p>{balls}</p>
+	      <div className='StartBut'>
+		  {button}
+		</div>
+	    </div>
+	);
+    }
+}
+
+function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export default Game
